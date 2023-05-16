f68e5b82fc98041204439c3e2b30ba94fc91f508
KhouloudChaabani <khouloudchaabani33@gmail.com>
2023-05-16 15:38:14 +0100

    hello world

---
+++ week1/fundamentals/fundamentals/practice/hello_world.py
@@ -0,0 +1,15 @@
+print("hello world ")
+name = "Khouloud"
+print("Hello",name)
+name = "Khouloud"
+print("Hello "+name)
+number=5
+print("Hello",number)
+number="8"
+print("Hello "+ number)
+food1="pizza"
+food2="crepe"
+print("I love to eat {} and {}".format(food1,food2 ))
+food1="pizza"
+food2="crepe"
+print(f"I love to eat {food1} and {food2}")
